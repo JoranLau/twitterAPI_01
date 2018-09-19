81ee7c25f22e4fc319e446da519673d2b8a9ead0
JoranLau <43103125+JoranLau@users.noreply.github.com>
2018-09-19 00:34:06 -0400

    Add files via upload

--- recon_pic.py
+++ recon_pic.py
@@ -9,19 +9,23 @@ from google.cloud.vision import types
 # Instantiates a client
 client = vision.ImageAnnotatorClient()
 
+for x in range(1,11):
 # The name of the image file to annotate
-file_name = '/media/sf_EC601_Product_Design/twitter_pics/8.jpg'
+	file_name = '/media/sf_EC601_Product_Design/twitter_pics/%d.jpg'%x
 
 # Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
+	with io.open(file_name, 'rb') as image_file:
+		content = image_file.read()
 
-image = types.Image(content=content)
+	image = types.Image(content=content)
 
 # Performs label detection on the image file
-response = client.label_detection(image=image)
-labels = response.label_annotations
+	response = client.label_detection(image=image)
+	labels = response.label_annotations
 
-print('Labels:')
-for label in labels:
-    print(label.description)
+	f = open("/media/sf_EC601_Product_Design/twitter_pics/%drecog.txt"%x, "w")
+	print(('pic%d_Labels:'%x+'\n'),file = f )
+	for label in labels:
+		print((label.description),file = f )
+	print(('\n'),file = f)
+	f.close()
